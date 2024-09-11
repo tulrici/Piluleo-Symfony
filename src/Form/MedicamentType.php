c7129eb7c70a733bbe479f5043269e9b9436072b
Thomas ULRICI <t.ulrici@it-students.fr>
2024-09-11 10:59:34 +0200

    CRUD Medicaments

---
+++ src/Form/MedicamentType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Medicament;
+use App\Entity\Ordonnance;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MedicamentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idMedicament')
+            ->add('description')
+            ->add('posologieOrdonnance')
+            ->add('ordonnance', EntityType::class, [
+                'class' => Ordonnance::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Medicament::class,
+        ]);
+    }
+}
